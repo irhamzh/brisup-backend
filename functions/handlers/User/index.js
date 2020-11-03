55dbbce5b94a1d63dec9cd5be5adbe874f97aeb2
임나연 <ammar.fiky@gmail.com>
2020-11-03 11:07:50 +0700

    chore: :heavy_plus_sign: add express-kun

--- functions/handlers/User/index.js
+++ functions/handlers/User/index.js
@@ -1,180 +1,191 @@
-const firebase = require('firebase')
-const { admin, db } = require('../../utils/admin')
-const config = require('../../utils/config')
-const formatISO = require('date-fns/formatISO')
+const firebase = require("firebase");
+const { admin, db } = require("../../utils/admin");
+const config = require("../../utils/config");
+const formatISO = require("date-fns/formatISO");
 
 exports.signup = (req, res) => {
-  const {email, password} = req.body
-  const defaultImg = 'no-user-pic.png'
+  const { email, password } = req.body;
+  const defaultImg = "no-user-pic.png";
   // TODO Validate Data
   // If email not email and if password is not valid
 
   let token, userId, newUser;
-  firebase.auth().createUserWithEmailAndPassword(email, password)
-  .then(data => {
-    userId = data.user.uid
-    return data.user.getIdToken()
-  })
-  .then(idToken => {
-    token = idToken
-    newUser = {
-      userId,
-      email,
-      imageUrl: `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${defaultImg}?alt=media`,
-      createdAt: formatISO(new Date(), {locale: 'id'}),
-    }
-
-    return db.doc(`users/${userId}`).set(newUser)
-  })
-  .then(() => {
-    return res.status(201).json({
-      success: true,
-      userId,
-      email: newUser.email,
-      imageUrl: newUser.imageUrl,
-      createdAt: newUser.createdAt,
-      token
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then((data) => {
+      userId = data.user.uid;
+      return data.user.getIdToken();
     })
-  })
-  .catch(err => {
-    console.error(err)
-    return res.status(500).json({success: false, message: err.message})
-  })
-}
+    .then((idToken) => {
+      token = idToken;
+      newUser = {
+        userId,
+        email,
+        imageUrl: `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${defaultImg}?alt=media`,
+        createdAt: formatISO(new Date(), { locale: "id" }),
+      };
+
+      return db.doc(`users/${userId}`).set(newUser);
+    })
+    .then(() => {
+      return res.status(201).json({
+        success: true,
+        userId,
+        email: newUser.email,
+        imageUrl: newUser.imageUrl,
+        createdAt: newUser.createdAt,
+        token,
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({ success: false, message: err.message });
+    });
+};
 
 exports.login = (req, res) => {
-  const {email, password} = req.body
+  const { email, password } = req.body;
 
-  firebase.auth().signInWithEmailAndPassword(email, password)
-  .then(data => {
-    return data.user.getIdToken()
-  })
-  .then(token => {
-    return res.json({success: true, email, token})
-  })
-  .catch(err => {
-    console.error(err)
-    return res.status(500).json({success: false, message: err.message})
-  })
-}
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then((data) => {
+      return data.user.getIdToken();
+    })
+    .then((token) => {
+      return res.json({ success: true, email, token });
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({ success: false, message: err.message });
+    });
+};
 
 // Get User Data
 exports.getUserData = (req, res) => {
-  let userData = {}
-  const {uid} = req.params
+  let userData = {};
+  const { uid } = req.params;
 
   db.doc(`/users/${uid}`)
-  .get()
-  .then(doc => {
-    if(doc.exists) {
-      userData = doc.data()
-      return db
-      .collection('roles')
-      .where('roleId', '==', userData.roleId)
-      .limit(1)
-      .get()
-    } else {
-      return res.status(404).json({success: false, message: 'Data tidak ditemukan'})
-    }
-  })
-  .then(data => {
-    userData.role = {}
-    data.forEach(doc => userData.role = doc.data())
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        userData = doc.data();
+        return db
+          .collection("roles")
+          .where("roleId", "==", userData.roleId)
+          .limit(1)
+          .get();
+      } else {
+        return res
+          .status(404)
+          .json({ success: false, message: "Data tidak ditemukan" });
+      }
+    })
+    .then((data) => {
+      userData.role = {};
+      data.forEach((doc) => (userData.role = doc.data()));
 
-    return res.json({success: true, data: userData})
-  })
-  .catch(err => {
-    console.error(err)
-    return res.status(500).json({success: false, message: err.message})
-  })
-}
+      return res.json({ success: true, data: userData });
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({ success: false, message: err.message });
+    });
+};
 
 // Get All Users
 exports.getAllUser = (req, res) => {
-  let userData = []
-  db
-  .collection('users')
-  .orderBy('createdAt', 'desc')
-  .get()
-  .then(data => {
-    data.forEach(doc => {
-      userData.push(doc.data())
+  let userData = [];
+  db.collection("users")
+    .orderBy("createdAt", "desc")
+    .get()
+    .then((data) => {
+      data.forEach((doc) => {
+        userData.push(doc.data());
+      });
+      return res.json({ success: true, data: userData });
     })
-    return res.json({success: true, data: userData})
-  })
-  .catch(err => {
-    console.error(err)
-    return res.status(500).json({success: false, message: err.message})
-  })
-}
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({ success: false, message: err.message });
+    });
+};
 
 exports.updateUser = (req, res) => {
-  const { nama} = req.body
+  const { nama } = req.body;
 
   const userDetails = {
     nama,
-    updatedAt: formatISO(new Date(), {locale: 'id'})
-  }
-  
+    updatedAt: formatISO(new Date(), { locale: "id" }),
+  };
+
   db.doc(`/users/${req.user.uid}`)
-  .update(userDetails)
-  .then(() => {
-    return res.json({ success: true, message: "Data berhasil diupdate!" })
-  })
-  .catch(err => {
-    console.error(err)
-    return res.status(500).json({success: false, message: err.message})
-  })
-}
+    .update(userDetails)
+    .then(() => {
+      return res.json({ success: true, message: "Data berhasil diupdate!" });
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({ success: false, message: err.message });
+    });
+};
 
 exports.uploadImage = (req, res) => {
-  const BusBoy = require('busboy')
-  const path = require('path')
-  const os = require('os')
-  const fs = require('fs')
-  const { v4: uuidv4 } = require('uuid');
+  const BusBoy = require("busboy");
+  const path = require("path");
+  const os = require("os");
+  const fs = require("fs");
+  const { v4: uuidv4 } = require("uuid");
 
-  const busboy = new BusBoy({headers: req.headers})
+  const busboy = new BusBoy({ headers: req.headers });
   let imageFileName;
-  let imageToBeUploaded = {}
+  let imageToBeUploaded = {};
 
-  busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
-    
-    if(mimetype !== 'image/jpeg' && mimetype !== 'image/png') {
-      return res.status(400).json({ success: false, message: 'Tipe file upload salah!' })
+  busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
+    if (mimetype !== "image/jpeg" && mimetype !== "image/png") {
+      return res
+        .status(400)
+        .json({ success: false, message: "Tipe file upload salah!" });
     }
 
-    const imageExtension = filename.split('.')[filename.split('.').length - 1]
-    imageFileName = filename
-    const filepath = path.join(os.tmpdir(), imageFileName)
-    imageToBeUploaded = {filepath, mimetype}
-    file.pipe(fs.createWriteStream(filepath))
-  })
+    const imageExtension = filename.split(".")[filename.split(".").length - 1];
+    imageFileName = filename;
+    const filepath = path.join(os.tmpdir(), imageFileName);
+    imageToBeUploaded = { filepath, mimetype };
+    file.pipe(fs.createWriteStream(filepath));
+  });
 
-  busboy.on('finish', () => {
-    admin.storage().bucket(config.storageBucket).upload(imageToBeUploaded.filepath, {
-      resumable: false,
-      destination: `images/${req.user.uid}/${imageFileName}`,
-      metadata: {
+  busboy.on("finish", () => {
+    admin
+      .storage()
+      .bucket(config.storageBucket)
+      .upload(imageToBeUploaded.filepath, {
+        resumable: false,
+        destination: `images/${req.user.uid}/${imageFileName}`,
         metadata: {
-          contentType: imageToBeUploaded.mimetype,
-          firebaseStorageDownloadTokens: uuidv4(),
-        }
-      }
-    })
-    .then(() => {
-      const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/images%2F${req.user.uid}%2F${imageFileName}?alt=media`
-      return db.doc(`/users/${req.user.uid}`).update({imageUrl})
+          metadata: {
+            contentType: imageToBeUploaded.mimetype,
+            firebaseStorageDownloadTokens: uuidv4(),
+          },
+        },
+      })
+      .then(() => {
+        const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/images%2F${req.user.uid}%2F${imageFileName}?alt=media`;
+        return db.doc(`/users/${req.user.uid}`).update({ imageUrl });
+      })
+      .then(() => {
+        return res.json({
+          success: true,
+          message: "Gambar berhasil diupload!",
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+        return res.status(500).json({ success: false, message: err.message });
+      });
+  });
 
-    })
-    .then(() => {
-      return res.json({success: true, message: 'Gambar berhasil diupload!'})
-    })
-    .catch(err => {
-      console.error(err)
-      return res.status(500).json({success: false, message: err.message})
-    })
-  })
-
-  busboy.end(req.rawBody)
-}
+  busboy.end(req.rawBody);
+};
--- functions/index.js
+++ functions/index.js
@@ -1,59 +1,85 @@
-const functions = require('firebase-functions');
-const app = require('express')()
-const firebase = require('firebase')
+const functions = require("firebase-functions");
+const app = require("express")();
+const firebase = require("firebase");
 
-const {admin, db} = require('./utils/admin');
-const { signup, login, uploadImage, updateUser, getUserData, getAllUser } = require('./handlers/User');
-const { FBAuth } = require('./middleware');
-const { getAllPersekot, createPersekot, updatePersekot, getPersekot } = require('./handlers/Persekot');
-const config = require('./utils/config');
-const { getAllVendor, getVendor, createVendor } = require('./handlers/Vendor');
-const { getAllRoles, getRole, createRole, updateRole } = require('./handlers/Roles');
-const { getAllMasterKondisi, getMasterKondisi, createMasterKondisi, updateMasterKondisi } = require('./handlers/MasterKondisi');
-const { getAllJenisBarang, getJenisBarang, createJenisBarang, updateJenisBarang } = require('./handlers/JenisBarang');
+const { admin, db } = require("./utils/admin");
+const {
+  signup,
+  login,
+  uploadImage,
+  updateUser,
+  getUserData,
+  getAllUser,
+} = require("./handlers/User");
+const { FBAuth } = require("./middleware");
+const {
+  getAllPersekot,
+  createPersekot,
+  updatePersekot,
+  getPersekot,
+} = require("./handlers/Persekot");
+const config = require("./utils/config");
+const { getAllVendor, getVendor, createVendor } = require("./handlers/Vendor");
+const {
+  getAllRoles,
+  getRole,
+  createRole,
+  updateRole,
+} = require("./handlers/Roles");
+const {
+  getAllMasterKondisi,
+  getMasterKondisi,
+  createMasterKondisi,
+  updateMasterKondisi,
+} = require("./handlers/MasterKondisi");
+const {
+  getAllJenisBarang,
+  getJenisBarang,
+  createJenisBarang,
+  updateJenisBarang,
+} = require("./handlers/JenisBarang");
 
-firebase.initializeApp(config)
+firebase.initializeApp(config);
 
 // User Route
-app.post('/signup', signup)
-app.post('/login', login)
-app.post('/user/image', FBAuth, uploadImage)
-app.post('/user/update', FBAuth, updateUser)
-app.get('/user/:uid', getUserData)
-app.get('/user', getAllUser)
+app.post("/signup", signup);
+app.post("/login", login);
+app.post("/user/image", FBAuth, uploadImage);
+app.post("/user/update", FBAuth, updateUser);
+app.get("/user/:uid", getUserData);
+app.get("/user", getAllUser);
 
 // Persekot Route
-app.get('/persekot', getAllPersekot)
-app.get('/persekot/:id', getPersekot)
-app.post('/persekot', FBAuth, createPersekot)
-app.put('/persekot/:id', FBAuth, updatePersekot)
+app.get("/persekot", getAllPersekot);
+app.get("/persekot/:id", getPersekot);
+app.post("/persekot", FBAuth, createPersekot);
+app.put("/persekot/:id", FBAuth, updatePersekot);
 
 // Vendor Route
-app.get('/vendor', getAllVendor)
-app.get('/vendor/:id', getVendor)
-app.post('/vendor', FBAuth, createVendor)
-app.put('/vendor/:id', FBAuth, updatePersekot)
+app.get("/vendor", getAllVendor);
+app.get("/vendor/:id", getVendor);
+app.post("/vendor", FBAuth, createVendor);
+app.put("/vendor/:id", FBAuth, updatePersekot);
 
 // Roles Route
-app.get('/roles', getAllRoles)
-app.get('/roles/:id', getRole)
-app.post('/roles', FBAuth, createRole)
-app.put('/roles/:id', FBAuth, updateRole)
+app.get("/roles", getAllRoles);
+app.get("/roles/:id", getRole);
+app.post("/roles", FBAuth, createRole);
+app.put("/roles/:id", FBAuth, updateRole);
 
 // masterKondisi Route
-app.get('/master-kondisi', getAllMasterKondisi)
-app.get('/master-kondisi/:id', getMasterKondisi)
-app.post('/master-kondisi', FBAuth, createMasterKondisi)
-app.put('/master-kondisi/:id', FBAuth, updateMasterKondisi)
+app.get("/master-kondisi", getAllMasterKondisi);
+app.get("/master-kondisi/:id", getMasterKondisi);
+app.post("/master-kondisi", FBAuth, createMasterKondisi);
+app.put("/master-kondisi/:id", FBAuth, updateMasterKondisi);
 
 // JenisBarang Route
-app.get('/master-kondisi', getAllJenisBarang)
-app.get('/master-kondisi/:id', getJenisBarang)
-app.post('/master-kondisi', FBAuth, createJenisBarang)
-app.put('/master-kondisi/:id', FBAuth, updateJenisBarang)
+app.get("/master-kondisi", getAllJenisBarang);
+app.get("/master-kondisi/:id", getJenisBarang);
+app.post("/master-kondisi", FBAuth, createJenisBarang);
+app.put("/master-kondisi/:id", FBAuth, updateJenisBarang);
 
 // Master Jenis Barang Route
 // app.get('/master-jenis-barang', getAllMasterJenisBarang)
 
-
-exports.api = functions.region('asia-southeast2').https.onRequest(app)
+exports.api = functions.region("asia-southeast2").https.onRequest(app);
