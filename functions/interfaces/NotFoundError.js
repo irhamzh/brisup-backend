b5923f9d142aa0403f05e25839e0491afab7cc02
임나연 <ammar.fiky@gmail.com>
2020-11-03 11:20:46 +0700

    refactor: :heavy_plus_sign: add interfaces

---
+++ functions/interfaces/NotFoundError.js
@@ -0,0 +1,12 @@
+class NotFoundError extends Error {
+  constructor(message, field) {
+    super(message);
+    this.field = field;
+    this.name = "NotFoundError";
+    Object.setPrototypeOf(this, NotFoundError.prototype);
+  }
+}
+
+NotFoundError.prototype.constructor = NotFoundError;
+
+export default NotFoundError;
