e5446c031f2c0e346d2ef91052a34c307dfedf19
임나연 <ammar.fiky@gmail.com>
2020-11-03 11:21:11 +0700

    refactor: add constant validationWording

---
+++ functions/constants/validationWording.js
@@ -0,0 +1,11 @@
+export default {
+  invalid: (name) => `${name} tidak valid`,
+  minLength: (min) => `Password minimal ${min} angka`,
+  minLength: (min) => `Password maksimal ${min} angka`,
+  required: (name) => `${name} tidak boleh kosong`,
+  notFound: (name) => `${name} tidak ditemukan`,
+  incorrect: (name) => `${name} salah`,
+  duplicate: (name, value) => `${name} ${value} telah dipakai`,
+  oneOf: (name, ...args) =>
+    `${name} harus salah satu diantara ${args.map((v) => `'${v}'`).join(", ")}`,
+};
