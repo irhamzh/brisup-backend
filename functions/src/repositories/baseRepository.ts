6662a754fa8643d97de3d005b55b2279df47d25e
임나연 <ammar.fiky@gmail.com>
2020-11-16 03:01:35 +0700

    refactor: add base repository and enum value handler

--- functions/src/repositories/baseRepository.ts
+++ functions/src/repositories/baseRepository.ts
@@ -1,48 +1,140 @@
+import * as admin from 'firebase-admin';
 import * as firebase from 'firebase';
+import { formatDateTime } from '@utils/Date';
+import NotFoundError from '@interfaces/NotFoundError';
+import validationWording from '@constants/validationWording';
+
+import { db } from '@utils/admin';
+
 export default class FirestoreRepository<CreateParam, ConditionParam = {}> {
-  _collection: firebase.firestore.CollectionReference;
-  _defaultPopulate: string[];
+  _collection: admin.firestore.CollectionReference;
+  _name: string;
+  // _defaultPopulate: string[];
   constructor(
-    db: firebase.firestore.Firestore,
+    // db: admin.firestore.Firestore,
     collectionName: string,
-    defaultPopulate: string[] = []
+    name: string
+    // defaultPopulate: string[] = []
   ) {
     this._collection = db.collection(collectionName);
-    this._defaultPopulate = defaultPopulate;
+    this._name = name;
+    // this._defaultPopulate = defaultPopulate;
   }
 
   async create(object: CreateParam) {
-    return await this._collection.add(object);
+    const createParam = {
+      ...object,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+    const ref = await this._collection.add(createParam);
+    const snap = await ref.get();
+    if (snap.data()) {
+      return {
+        id: ref.id,
+        ...snap.data(),
+        createdAt: formatDateTime(snap.data()?.createdAt.toDate()),
+        updatedAt: formatDateTime(snap.data()?.updatedAt.toDate()),
+      };
+    }
+    return {
+      id: ref.id,
+      ...object,
+      createdAt: formatDateTime(createParam.createdAt),
+      updatedAt: formatDateTime(createParam.updatedAt),
+    };
   }
 
-  async readOne(id: string) {
-    return await this._collection.doc(id);
+  async findById(id: string) {
+    const ref: admin.firestore.DocumentReference = this._collection.doc(id);
+    const snap: admin.firestore.DocumentSnapshot = await ref.get();
+    if (!snap.exists) {
+      throw new NotFoundError(
+        validationWording.notFound(this._name),
+        this._name
+      );
+    }
+    const data = snap.data();
+    return {
+      id: snap.id,
+      ...data,
+      createdAt: formatDateTime(data?.createdAt.toDate()),
+      updatedAt: formatDateTime(data?.updatedAt.toDate()),
+    };
   }
 
-  async readAll() {
-    const execute: any = await this._collection
-      .orderBy('createdAt', 'desc')
+  async findAll(page: number | string = 1, limit: number | string = 5) {
+    // .where('name', '==', '\uf8ff' + '' + '\uf8ff')
+    const parsedPage = parseInt(page as string);
+    const parsedLimit = parseInt(limit as string);
+    const skip = (parsedPage - 1) * parsedLimit || 1;
+
+    //get skipbatch
+    const first = await this._collection
+      .orderBy('createdAt', 'asc')
+      .limit(skip)
       .get();
-    const data = execute.map((item: firebase.firestore.DocumentData) => ({
-      id: item,
-      ...item.data(),
-    }));
+    if (first.docs.length <= 0) {
+      return [];
+    }
+    const last = first.docs[first.docs.length - 1];
+
+    //getData
+    const ref = await this._collection
+      .orderBy('createdAt', 'asc')
+      .startAt(last)
+      .limit(parsedLimit)
+      .get();
+    const data: admin.firestore.DocumentData = [];
+    ref.forEach((doc: firebase.firestore.DocumentData) => {
+      const snap = { id: doc.id, ...doc.data() };
+      return data.push({
+        ...snap,
+        createdAt: formatDateTime(snap.createdAt.toDate()),
+        updatedAt: formatDateTime(snap.updatedAt.toDate()),
+      });
+    });
     return data;
   }
 
-  async update(id: string, object: CreateParam) {
-    const ref: firebase.firestore.DocumentReference = this._collection.doc(id);
-    const snap: firebase.firestore.DocumentSnapshot = await ref.get();
+  async update(id: string, object: Partial<CreateParam>) {
+    const ref: admin.firestore.DocumentReference = this._collection.doc(id);
+    const snap: admin.firestore.DocumentSnapshot = await ref.get();
     if (!snap.exists) {
-      return ref;
+      throw new NotFoundError(
+        validationWording.notFound(this._name),
+        this._name
+      );
     }
-    await ref.update(object);
-    return ref;
+    const createParam = {
+      ...object,
+      updatedAt: new Date(),
+    };
+    await ref.set(createParam, { merge: true });
+    const updateSnap = await ref.get();
+    return {
+      id: ref.id,
+      ...updateSnap.data(),
+      createdAt: formatDateTime(updateSnap.data()?.createdAt.toDate()),
+      updatedAt: formatDateTime(updateSnap.data()?.updatedAt.toDate()),
+    };
   }
 
   async delete(id: string) {
-    const ref: firebase.firestore.DocumentReference = this._collection.doc(id);
+    const ref: admin.firestore.DocumentReference = this._collection.doc(id);
+    const snap: admin.firestore.DocumentSnapshot = await ref.get();
+    if (!snap.exists) {
+      throw new NotFoundError(
+        validationWording.notFound(this._name),
+        this._name
+      );
+    }
     await ref.delete();
-    return ref;
+    return {
+      id: ref.id,
+      ...snap.data(),
+      createdAt: formatDateTime(snap.data()?.createdAt.toDate()),
+      updatedAt: formatDateTime(snap.data()?.updatedAt.toDate()),
+    };
   }
 }
