023afc91c6e88a3f37d74b154a13347e89287ff0
임나연 <ammar.fiky@gmail.com>
2020-11-16 03:00:52 +0700

    refactor: add paramValidaion

--- functions/src/utils/paramValidation.ts
+++ functions/src/utils/paramValidation.ts
@@ -1,9 +1,12 @@
 import * as yup from 'yup';
 import validationWording from '@constants/validationWording';
 
-export default function (params: string, field = 'uid') {
-  const paramValidationData = yup.object().shape({
-    uid: yup.string().required(validationWording.required(field)),
-  });
+export default function (params: any, field = 'uid') {
+  const paramValidationData = yup
+    .object()
+    .shape({
+      uid: yup.string().required(validationWording.required(field)),
+    })
+    .required();
   return paramValidationData.validateSync(params);
 }
