c666f1cee56a09889765101c416c4020a463ca22
임나연 <ammar.fiky@gmail.com>
2020-11-17 11:31:51 +0700

    refactor: add base interface

---
+++ functions/src/interfaces/BaseInterface.ts
@@ -0,0 +1,15 @@
+export enum BaseCondition {
+  good = 'Baik',
+  undefined = 'Belum Ditentukan',
+  bad = 'Buruk',
+}
+
+export enum YesNo {
+  yes = 'Ya',
+  no = 'Tidak',
+}
+
+export enum JenisAnggatan {
+  investasi = 'Investasi',
+  eksploitasi = 'Eksploitasi',
+}
