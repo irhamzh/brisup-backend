43815f9c178deb8ea9878b1169aac4d7df0a82de
임나연 <ammar.fiky@gmail.com>
2020-11-23 16:37:42 +0700

    refactor: chekcbox

--- functions/src/modules/PengelolaanGedung/Kebersihan/sanitation.schema.ts
+++ functions/src/modules/PengelolaanGedung/Kebersihan/sanitation.schema.ts
@@ -1,6 +1,6 @@
 import * as yup from 'yup';
 import getAllEnumKey from '@utils/getAllEnumKeys';
-import { BaseCondition1 } from '@constants/BaseCondition';
+// import { BaseCondition1 } from '@constants/BaseCondition';
 import validationWording from '@constants/validationWording';
 import {
   TypeInnovationBuilding,
@@ -10,93 +10,19 @@ import {
 const yardSanitation = yup
   .object()
   .shape({
-    rumput: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf('rumput', ...getAllEnumKey(BaseCondition1))
-      ),
-    pohon: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf('pohon', ...getAllEnumKey(BaseCondition1))
-      ),
-    kolamIkan: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf('kolamIkan', ...getAllEnumKey(BaseCondition1))
-      ),
-    airMancur: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf('airMancur', ...getAllEnumKey(BaseCondition1))
-      ),
-    pavingBlock: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf('pavingBlock', ...getAllEnumKey(BaseCondition1))
-      ),
-    sampahGulma: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf('sampahGulma', ...getAllEnumKey(BaseCondition1))
-      ),
-    penyiraman: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf('penyiraman', ...getAllEnumKey(BaseCondition1))
-      ),
-    pendangiran: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf('pendangiran', ...getAllEnumKey(BaseCondition1))
-      ),
-    pemupukan: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf('pemupukan', ...getAllEnumKey(BaseCondition1))
-      ),
-    pemangkasan: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf('pemangkasan', ...getAllEnumKey(BaseCondition1))
-      ),
-    pengendalianHama: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf(
-          'pengendalianHama',
-          ...getAllEnumKey(BaseCondition1)
-        )
-      ),
-    penyulamanTanaman: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf(
-          'penyulamanTanaman',
-          ...getAllEnumKey(BaseCondition1)
-        )
-      ),
-    penambahanMediaTanam: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf(
-          'penambahanMediaTanam',
-          ...getAllEnumKey(BaseCondition1)
-        )
-      ),
+    rumput: yup.boolean(),
+    pohon: yup.boolean(),
+    kolamIkan: yup.boolean(),
+    airMancur: yup.boolean(),
+    pavingBlock: yup.boolean(),
+    sampahGulma: yup.boolean(),
+    penyiraman: yup.boolean(),
+    pendangiran: yup.boolean(),
+    pemupukan: yup.boolean(),
+    pemangkasan: yup.boolean(),
+    pengendalianHama: yup.boolean(),
+    penyulamanTanaman: yup.boolean(),
+    penambahanMediaTanam: yup.boolean(),
   })
   .required();
 
@@ -123,54 +49,14 @@ const updateYardSanitation = yup
 const smartBuildingSanitation = yup
   .object()
   .shape({
-    plafond: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf('plafond', ...getAllEnumKey(BaseCondition1))
-      ),
-    dinding: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf('dinding', ...getAllEnumKey(BaseCondition1))
-      ),
-    lantai: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf('lantai', ...getAllEnumKey(BaseCondition1))
-      ),
-    pintu: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf('pintu', ...getAllEnumKey(BaseCondition1))
-      ),
-    jendela: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf('jendela', ...getAllEnumKey(BaseCondition1))
-      ),
-    kursi: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf('kursi', ...getAllEnumKey(BaseCondition1))
-      ),
-    meja: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf('meja', ...getAllEnumKey(BaseCondition1))
-      ),
-    lampu: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf('lampu', ...getAllEnumKey(BaseCondition1))
-      ),
+    plafond: yup.boolean(),
+    dinding: yup.boolean(),
+    lantai: yup.boolean(),
+    pintu: yup.boolean(),
+    jendela: yup.boolean(),
+    kursi: yup.boolean(),
+    meja: yup.boolean(),
+    lampu: yup.boolean(),
   })
   .required();
 
@@ -205,105 +91,27 @@ const updateSmartBuildingSanitation = yup
 const saranaPendukungMusholaSanitation = yup
   .object()
   .shape({
-    dinding: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf('dinding', ...getAllEnumKey(BaseCondition1))
-      ),
-    pintu: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf('pintu', ...getAllEnumKey(BaseCondition1))
-      ),
-    ceiling: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf('ceiling', ...getAllEnumKey(BaseCondition1))
-      ),
-    carpetSholat: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf(
-          'carpetSholat',
-          ...getAllEnumKey(BaseCondition1)
-        )
-      ),
-    sajadah: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf('sajadah', ...getAllEnumKey(BaseCondition1))
-      ),
-    jamDinding: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf('jamDinding', ...getAllEnumKey(BaseCondition1))
-      ),
-    pajangan: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf('pajangan', ...getAllEnumKey(BaseCondition1))
-      ),
+    dinding: yup.boolean(),
+    pintu: yup.boolean(),
+    ceiling: yup.boolean(),
+    carpetSholat: yup.boolean(),
+    sajadah: yup.boolean(),
+    jamDinding: yup.boolean(),
+    pajangan: yup.boolean(),
   })
   .required();
 
 const saranaPendukungSecurityPosSanitation = yup
   .object()
   .shape({
-    dinding: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf('dinding', ...getAllEnumKey(BaseCondition1))
-      ),
-    pintu: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf('pintu', ...getAllEnumKey(BaseCondition1))
-      ),
-    plafond: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf('plafond', ...getAllEnumKey(BaseCondition1))
-      ),
-    lantai: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf('lantai', ...getAllEnumKey(BaseCondition1))
-      ),
-    jendela: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf('jendela', ...getAllEnumKey(BaseCondition1))
-      ),
-    kursi: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf('kursi', ...getAllEnumKey(BaseCondition1))
-      ),
-    meja: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf('meja', ...getAllEnumKey(BaseCondition1))
-      ),
-    lampu: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf('lampu', ...getAllEnumKey(BaseCondition1))
-      ),
+    dinding: yup.boolean(),
+    pintu: yup.boolean(),
+    plafond: yup.boolean(),
+    lantai: yup.boolean(),
+    jendela: yup.boolean(),
+    kursi: yup.boolean(),
+    meja: yup.boolean(),
+    lampu: yup.boolean(),
   })
   .required();
 
@@ -419,54 +227,14 @@ const updateLocation = yup
 const baseRuangan = yup
   .object()
   .shape({
-    plafond: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf('plafond', ...getAllEnumKey(BaseCondition1))
-      ),
-    dinding: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf('dinding', ...getAllEnumKey(BaseCondition1))
-      ),
-    lantai: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf('lantai', ...getAllEnumKey(BaseCondition1))
-      ),
-    pintu: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf('pintu', ...getAllEnumKey(BaseCondition1))
-      ),
-    jendela: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf('jendela', ...getAllEnumKey(BaseCondition1))
-      ),
-    kursi: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf('kursi', ...getAllEnumKey(BaseCondition1))
-      ),
-    meja: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf('meja', ...getAllEnumKey(BaseCondition1))
-      ),
-    lampu: yup
-      .mixed<keyof typeof BaseCondition1>()
-      .oneOf(
-        getAllEnumKey(BaseCondition1),
-        validationWording.oneOf('lampu', ...getAllEnumKey(BaseCondition1))
-      ),
+    plafond: yup.boolean(),
+    dinding: yup.boolean(),
+    lantai: yup.boolean(),
+    pintu: yup.boolean(),
+    jendela: yup.boolean(),
+    kursi: yup.boolean(),
+    meja: yup.boolean(),
+    lampu: yup.boolean(),
   })
   .required();
 
