708418f8340a23e041c49ddb1fb78ed5c5076fba
임나연 <ammar.fiky@gmail.com>
2020-12-28 01:12:45 +0700

    refactor: add role access

--- functions/src/modules/MasterData/Role/interface/role.interface.ts
+++ functions/src/modules/MasterData/Role/interface/role.interface.ts
@@ -1,5 +1,19 @@
+interface IRoleAcceess {
+  create: boolean;
+  update: boolean;
+  delete: boolean;
+  read: boolean;
+  dashboard: boolean;
+  approvalKabag: boolean;
+  approvalWakabag: boolean;
+}
+
 export interface IRoleBase {
   name: string;
+  fixedAsset: IRoleAcceess;
+  procurement: IRoleAcceess;
+  generalAffair: IRoleAcceess;
+  financialAdmin: IRoleAcceess;
   createdAt?: Date;
   updatedAt?: Date;
 }
