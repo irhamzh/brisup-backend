9f3f18f2f80fb928d075084080ae494f6b9ea464
임나연 <ammar.fiky@gmail.com>
2021-01-06 21:58:02 +0700

    feat: :sparkles: persekot new

--- functions/src/modules/FixedAsset/Persekot/interface/persekot.interface.ts
+++ functions/src/modules/FixedAsset/Persekot/interface/persekot.interface.ts
@@ -1,12 +1,48 @@
+interface IApprovalLog {
+  date: Date;
+  userId: string;
+  name: string;
+  role: string;
+  status: string;
+}
+
 export interface IPersekotBase {
   date: Date;
   name: string;
   division: string;
   costNominal: number;
+  status: string;
+  approvalLog: IApprovalLog[];
   information?: string;
-  typePersekot?: string;
   createdAt?: Date;
   updatedAt?: Date;
 }
 
+export interface IPersekotFinancialAdmin extends IPersekotBase {
+  typePersekot: string;
+}
+
 export const FinancialAdminPersekotType = ['LOP', 'DLK'];
+
+export enum ApprovalStatus {
+  'Unapproved' = 'Unapproved',
+  'Approved oleh Supervisor I' = 'Approved oleh Supervisor I',
+  'Diajukan Penihilan' = 'Diajukan Penihilan',
+  'Approved oleh Supervisor II' = 'Approved oleh Supervisor II',
+  'Approved oleh Wakabag' = 'Approved oleh Wakabag',
+  'Approved oleh Kabag' = 'Approved oleh Kabag',
+}
+
+export enum ApprovalNextStatus {
+  'Unapproved' = 'Approved oleh Supervisor I',
+  'Approved oleh Supervisor I' = 'Diajukan Penihilan',
+  'Diajukan Penihilan' = 'Approved oleh Supervisor II',
+}
+
+/**
+ * -> Unapproved
+ * -> Approved oleh Supervisor I
+ * -> Diajukan Penihilan
+ * -> Approved oleh Supervisor II
+ * -> Approved oleh Kabag /Approved oleh Wakabag
+ */
--- functions/src/modules/FixedAsset/Persekot/persekot.repository.ts
+++ functions/src/modules/FixedAsset/Persekot/persekot.repository.ts
@@ -1,9 +1,14 @@
 import BaseRepository from '@repositories/baseRepository';
-import { IPersekotBase } from '@modules/FixedAsset/Persekot/interface/persekot.interface';
+import {
+  IPersekotBase,
+  IPersekotFinancialAdmin,
+} from '@modules/FixedAsset/Persekot/interface/persekot.interface';
 import { db } from '@utils/admin';
 import * as admin from 'firebase-admin';
 
-export default class PersekotRepository extends BaseRepository<IPersekotBase> {
+export default class PersekotRepository extends BaseRepository<
+  IPersekotBase | IPersekotFinancialAdmin
+> {
   _persekotModel: admin.firestore.CollectionReference;
   constructor() {
     super('persekots', 'persekot');
@@ -11,6 +16,15 @@ export default class PersekotRepository extends BaseRepository<IPersekotBase> {
   }
 
   async deleteMultiple(persekotIds: string[]) {
+    const batch = db.batch();
+    for (const persekot of persekotIds) {
+      const ref = this._persekotModel.doc(persekot);
+      batch.delete(ref);
+    }
+    return batch.commit();
+  }
+
+  async pengajuanPenihilan(persekotIds: string[]) {
     const batch = db.batch();
     for (const persekot of persekotIds) {
       const ref = this._persekotModel.doc(persekot);
