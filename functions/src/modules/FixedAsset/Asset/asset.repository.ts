86835e545db3e8080d9d31da2649d095e588a059
임나연 <ammar.fiky@gmail.com>
2021-01-11 15:19:47 +0700

    refactor: unused code

--- functions/src/modules/FixedAsset/Asset/asset.repository.ts
+++ functions/src/modules/FixedAsset/Asset/asset.repository.ts
@@ -14,11 +14,6 @@ export default class AssetRepository extends BaseRepository<IAssetBase> {
     this._assetModel = db.collection('assets');
   }
 
-  async getAssetArray(assetsIds: string[]) {
-    // const first = await this._collection
-    //   .where('country', 'in', ['USA', 'Japan']).get();
-    //   .get();
-  }
   async deleteMultiple(assetIds: string[]) {
     const batch = db.batch();
     for (const asset of assetIds) {
