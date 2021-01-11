5a6ee5f3f96745d944ec59769ca2f535015fda2c
임나연 <ammar.fiky@gmail.com>
2021-01-11 16:23:13 +0700

    feat: pauk approval

--- functions/src/modules/FinancialAdmin/Payment/payment.repository.ts
+++ functions/src/modules/FinancialAdmin/Payment/payment.repository.ts
@@ -1,4 +1,13 @@
+import * as admin from 'firebase-admin';
+
+import { db } from '@utils/admin';
+import { StringKeys } from '@interfaces/BaseInterface';
+import { ApprovalStatus } from '@constants/BaseCondition';
+import { IUserDecoded } from '@modules/MasterData/User/interface/user.interface';
+
 import BaseRepository from '@repositories/baseRepository';
+import validationWording from '@constants/validationWording';
+import { TypePayment } from './interface/payment.interface';
 
 import {
   IDataKelogistikan,
@@ -39,7 +48,81 @@ export default class PaymentRepositoryRepository extends BaseRepository<
   | IHotel
   | IAkomodasiAsrama
 > {
+  _paymentModel: admin.firestore.CollectionReference;
   constructor() {
     super('fa_payments', 'fa_payment');
+    this._paymentModel = db.collection('fa_payments');
+  }
+
+  async pengajuanPenihilan(paukIds: string[], user: IUserDecoded) {
+    const batchCommits = [];
+    const invalidRow: StringKeys[] = [];
+
+    let batch = db.batch();
+    for (let i = 0; i < paukIds.length; i++) {
+      // -> defined docref
+      const docRef = this._paymentModel.doc(paukIds[i]);
+
+      // -> check exist
+      // -> skp if not exist
+      const snap = await docRef.get();
+      if (!snap.exists) {
+        invalidRow.push({
+          id: paukIds[i],
+          error: validationWording.notFound('PAUK'),
+        });
+        continue;
+      }
+
+      // -> get data
+      const data = snap.data();
+
+      if (data?.typePayment !== TypePayment['Penihilan PAUK']) {
+        invalidRow.push({
+          id: paukIds[i],
+          error: validationWording.notFound('PAUK'),
+        });
+        continue;
+      }
+      if (
+        ApprovalStatus['Approved oleh Supervisor I'].toLowerCase() !==
+        data?.statusPenihilan?.toLowerCase()
+      ) {
+        invalidRow.push({
+          id: paukIds[i],
+          name: data?.name,
+          error: `Untuk diajukan ke Penihilan, PAUK harus berada dalam statusPenihilan "${ApprovalStatus['Approved oleh Supervisor I']}". Status sekarang "${data?.statusPenihilan}"`,
+        });
+        continue;
+      }
+
+      // -> Update Data
+      const log = {
+        statusPenihilan: ApprovalStatus['Diajukan Penihilan'],
+        date: new Date(),
+        userId: user.uid,
+        name: user.name,
+        role: user.role.name,
+      };
+      const approvalLogPenihilan = [...data.approvalLogPenihilan, log];
+
+      // -> execute update data
+      batch.set(
+        docRef,
+        {
+          statusPenihilan: ApprovalStatus['Diajukan Penihilan'],
+          approvalLogPenihilan,
+        },
+        { merge: true }
+      );
+      if ((i + 1) % 500 === 0) {
+        console.log(`Writing records[i] ${i + 1}`);
+        batchCommits.push(batch.commit());
+        batch = db.batch();
+      }
+    }
+    batchCommits.push(batch.commit());
+    await Promise.all(batchCommits);
+    return invalidRow;
   }
 }
