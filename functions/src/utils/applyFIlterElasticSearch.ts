bbc3aca22e36a52e61f69e363b1d276058144686
임나연 <ammar.fiky@gmail.com>
2021-01-17 01:15:55 +0700

    feat: :sparkles: apply elastic search filter

---
+++ functions/src/utils/applyFIlterElasticSearch.ts
@@ -0,0 +1,102 @@
+import {
+  startOfMonth,
+  endOfMonth,
+  startOfYear,
+  endOfYear,
+  startOfDay,
+  endOfDay,
+} from 'date-fns';
+
+import { filterParamAttributes } from '@interfaces/BaseInterface';
+
+export default function applyFilterElasticSearch(
+  filtered: filterParamAttributes[]
+) {
+  const defaultReturn = {
+    bool: {
+      must: [],
+    },
+  };
+  if (!filtered || filtered?.length < 1 || !filtered[0]?.id) {
+    return defaultReturn;
+  }
+
+  const filterBody: any[] = [];
+  for (const filterParam of filtered) {
+    let { id, value } = filterParam;
+    if (id.includes('$')) {
+      const [operatorQuery, fieldQuery] = id.split('$');
+      if (operatorQuery.toLocaleLowerCase() === 'in') {
+        filterBody.push({
+          terms: {
+            [fieldQuery]: value,
+          },
+        });
+      } else if (operatorQuery === 'month-year') {
+        const startDay = startOfMonth(new Date(value));
+        const endDay = endOfMonth(new Date(value));
+        filterBody.push({
+          range: {
+            [fieldQuery]: { gte: startDay, lte: endDay },
+          },
+        });
+      } else if (operatorQuery === 'year') {
+        const startDay = startOfYear(new Date(value));
+        const endDay = endOfYear(new Date(value));
+        filterBody.push({
+          range: {
+            [fieldQuery]: { gte: startDay, lte: endDay },
+          },
+        });
+      } else if (operatorQuery === 'atDate') {
+        const startDay = startOfDay(new Date(value));
+        const endDay = endOfDay(new Date(value));
+        filterBody.push({
+          range: {
+            [fieldQuery]: { gte: startDay, lte: endDay },
+          },
+        });
+      } else if (operatorQuery === 'beforeDate') {
+        const endDay = endOfDay(new Date(value));
+        filterBody.push({
+          range: {
+            [fieldQuery]: { lt: endDay },
+          },
+        });
+      } else if (operatorQuery === 'afterDate') {
+        const startDay = startOfDay(new Date(value));
+        filterBody.push({
+          range: {
+            [fieldQuery]: { gt: startDay },
+          },
+        });
+      } else if (operatorQuery === 'betweenDate') {
+        const startDay = startOfDay(new Date(value[0]));
+        const endDay = endOfDay(new Date(value[1]));
+        filterBody.push({
+          range: {
+            [fieldQuery]: { gte: startDay, lte: endDay },
+          },
+        });
+      } else {
+        filterBody.push({
+          [operatorQuery]: {
+            [fieldQuery]: value,
+          },
+        });
+      }
+    } else {
+      filterBody.push({
+        wildcard: {
+          [id]: `*${value}*`,
+        },
+      });
+    }
+  }
+  const queryParam = {
+    bool: {
+      must: filterBody,
+    },
+  };
+  return queryParam || defaultReturn;
+}
