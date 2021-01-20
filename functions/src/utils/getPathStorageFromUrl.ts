7bf03aab375db0e37b78218751bd0c96d9a8049d
임나연 <ammar.fiky@gmail.com>
2021-01-20 15:29:02 +0700

    feat: :sparkles: get path from url firebase

--- functions/src/utils/getPathStorageFromUrl.ts
+++ functions/src/utils/getPathStorageFromUrl.ts
@@ -1,9 +1,21 @@
 import { BASE_URL_STORAGE } from '@constants/config';
 
-export default function getPathStorageFromUrl(url: String) {
+function getSinglePathStorageFromUrl(url: string) {
   let imagePath = url.replace(BASE_URL_STORAGE, '');
   const indexOfEndPath = imagePath.indexOf('?');
   imagePath = imagePath.substring(0, indexOfEndPath);
   imagePath = imagePath.replace('%2F', '/');
   return decodeURIComponent(imagePath);
+}
+
+export default function getPathStorageFromUrl(url: string | string[]) {
+  const arrayImagePath = [];
+  if (url instanceof Array && Array.isArray(url)) {
+    for (const imageUrl of url) {
+      arrayImagePath.push(getSinglePathStorageFromUrl(imageUrl));
+    }
+  } else {
+    arrayImagePath.push(getSinglePathStorageFromUrl(url));
+  }
+  return arrayImagePath;
 }
