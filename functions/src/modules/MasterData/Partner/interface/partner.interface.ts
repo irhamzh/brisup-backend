744b47685c4b6f3a2918a5989e4ba32140f19fbb
임나연 <ammar.fiky@gmail.com>
2021-01-22 13:31:34 +0700

    refactor: :recycle: partner master data

--- functions/src/modules/MasterData/Partner/interface/partner.interface.ts
+++ functions/src/modules/MasterData/Partner/interface/partner.interface.ts
@@ -1,5 +1,20 @@
 export interface IPartnerBase {
   name: string;
+  address: string;
+  contact: string;
+  specialization: string[];
   createdAt?: Date;
   updatedAt?: Date;
 }
+
+export const Specialization = [
+  'Lift',
+  'Gondola',
+  'AHU System',
+  'Pest Control',
+  'Tanaman Hias',
+  'Genset System',
+  'Alat Elektrikal',
+  'Pewangi Ruangan',
+  'Pengangkutan Sampah',
+];
