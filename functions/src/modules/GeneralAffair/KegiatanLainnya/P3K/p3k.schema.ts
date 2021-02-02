e2f57b5317acaa4f7b3d2e8ccd82a8e45150c5a0
임나연 <ammar.fiky@gmail.com>
2021-02-02 16:14:49 +0700

    fix(General Affair): p3k area > lantai 1-6

--- functions/src/modules/GeneralAffair/KegiatanLainnya/P3K/p3k.schema.ts
+++ functions/src/modules/GeneralAffair/KegiatanLainnya/P3K/p3k.schema.ts
@@ -1,12 +1,16 @@
 import * as yup from 'yup';
 import validationWording from '@constants/validationWording';
+import { Lantai } from './interface/p3k.interface';
 
 const create = yup
   .object()
   .shape({
     tanggal: yup.date().required(validationWording.required('tanggal')),
     expired: yup.date().required(validationWording.required('expired')),
-    area: yup.string().required(validationWording.required('area')),
+    area: yup
+      .mixed()
+      .oneOf(Lantai)
+      .required(validationWording.required('area')),
     medicineType: yup
       .string()
       .required(validationWording.required('Medicine Type')),
@@ -22,7 +26,7 @@ const update = yup
   .shape({
     tanggal: yup.date(),
     expired: yup.date(),
-    area: yup.string(),
+    area: yup.mixed().oneOf(Lantai),
     medicineType: yup.string(),
     information: yup.string(),
     jumlah: yup.number(),
