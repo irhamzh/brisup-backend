dee631dba51b29d983c5aac2f12d4e026e481adc
임나연 <ammar.fiky@gmail.com>
2021-09-16 11:07:51 +0700

    fix: constant

--- functions/src/constants/config.ts
+++ functions/src/constants/config.ts
@@ -3,9 +3,5 @@ import config from '@utils/config';
 
 const BASE_URL_STORAGE = `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/`;
 
-const {
-  ES_HOST = '127.0.0.1',
-  ES_PORT = '9200',
-  ES_NODE = 'http://search-bri.ujiaplikasi.com',
-} = process.env;
+const { ES_HOST, ES_PORT, ES_NODE } = process.env;
 export { ES_HOST, ES_PORT, ES_NODE, BASE_URL_STORAGE };
